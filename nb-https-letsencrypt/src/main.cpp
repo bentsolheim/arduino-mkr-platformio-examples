46be65f5c3b1ddaaa7a7eaf6e42a5bad58fcee2e
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-09-17 22:00:36 +0200

    Simplified example and added some comments

--- nb-https-letsencrypt/src/main.cpp
+++ nb-https-letsencrypt/src/main.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 #include <MKRNB.h>
-#include <Wire.h>
-#include <BearSSLClient.h>
 #include <ArduinoBearSSL.h>
-#include "SdHttpClient.h"
 
 #if __has_include("arduino_secrets.h")
 
@@ -13,118 +10,178 @@
 #include "default_secrets.h"
 #endif
 
-
 const char PINNUMBER[] = SECRET_PINNUMBER;
+const char GPRS_APN[] = SECRET_GPRS_APN;
+const char GPRS_LOGIN[] = SECRET_GPRS_LOGIN;
+const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
 
-NBClient client;
-BearSSLClient sslClient(client);
-GPRS gprs;
 NB nbAccess(false);
-NBScanner nbScanner;
+NBClient nbClient;
 
-void sslConnect(const char *host, const char *path="/");
+// Create a BearSSLClient object, wrapping the NBClient. This is what will allow us to use
+// https connections without relying on the NBSSLClient in the MKRNB library.
+BearSSLClient sslClient(nbClient);
 
-void gprsConnect();
+static const int ERROR_SSL_CONNECT = 1;
+static const int ERROR_REQUEST_FAILED = 2;
+static const int ERROR_RESPONSE_DOWNLOAD = 3;
 
-String response;
+int downloadSsl(const char *host, const char *path = "/");
+
+bool gprsConnect(int attempts = 3, int delayMillis = 1000);
+
+int copyStream(Stream *source, Stream *target, int waitForBytesAttempts = 10, int waitForBytesDelayMillis = 10);
+
+bool performGet(Client *client, const char *host, const char *path);
+
+void printModemVersion();
 
 unsigned long getTime() {
     return nbAccess.getTime();
 }
 
 void setup() {
+    // Let's pull the builtin led down to prevent it from floating...
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
     Serial.begin(9600);
-    while (!Serial) { ; // wait for serial port to connect. Needed for native USB port only
+    while (!Serial) {}
+
+    Serial.println("Connecting to network...");
+
+    if (!gprsConnect()) {
+        Serial.print("Unable to connect to network. Aborting.");
+        return;
     }
-    Serial.println("Starting");
+    Serial.println("Connected to network successfully...");
 
-    gprsConnect();
+    printModemVersion();
 
+    // Important step. Let BearSSL know how to get the current time.
+    // Needed for certificate verification.
     ArduinoBearSSL.onGetTime(getTime);
-}
 
-void loop() {
-
-    Serial.println("\n\n\n");
-    delay(5000);
-
-    MODEM.send("AT+GMI");
-    MODEM.waitForResponse(100, &response);
-    Serial.println(response);
-
-    MODEM.send("AT+GMM");
-    MODEM.waitForResponse(100, &response);
-    Serial.println(response);
-
-    MODEM.send("AT+GMR");
-    MODEM.waitForResponse(100, &response);
-    Serial.println(response);
-
-    MODEM.send("ATI9");
-    MODEM.waitForResponse(100, &response);
-    Serial.println(response);
-
-    Serial.print("GPRS ready: ");
-    Serial.println(gprs.ready());
-
-    Serial.print("GPRS status: ");
-    Serial.println(gprs.status());
-
-    Serial.print("NB access ready: ");
-    Serial.println(nbAccess.ready());
-
-    Serial.print("NB access status: ");
-    Serial.println(nbAccess.status());
-
-    Serial.print("NB access alive: ");
-    Serial.println(nbAccess.isAccessAlive());
-
-    Serial.print("NB scanner carrier: ");
-    Serial.println(nbScanner.getCurrentCarrier());
-
-    Serial.print("NB scanner signal: ");
-    Serial.println(nbScanner.getSignalStrength());
-
-    Serial.println("\n\n");
-    sslConnect("letsencrypt.org");
-}
-
-void sslConnect(const char *host, const char *path) {
-    unsigned long start = millis();
-    Serial.print("Start connect ");
-    Serial.print(host);
-    Serial.print(" ... ");
-    bool connected = sslClient.connect(host, 443);
-    Serial.println("connected");
-    if (!connected) {
-        Serial.print("Unable to connect to ");
-        Serial.println(host);
-    } else {
-        Serial.print("Connected to ");
-        Serial.print(host);
-        Serial.print(" in ");
-        Serial.print(millis() - start);
-        Serial.println(" millis");
-
-        Serial.println("Prepare");
-        Serial.println(prepareGetRequest(&sslClient, host, path));
-        Serial.println("Download");
-        int status = downloadToStream(&sslClient, &Serial);
-        Serial.println();
+    // Download https://letsencrypt.org . This will not work using NBSSLClient on the
+    // u-blox SARA-R410M-02B modem with firmware/app version L0.0.00.00.05.06,A.02.00
+    // from Feb 03 2018. BearSSL to the rescue.
+    int status = downloadSsl("letsencrypt.org", "/");
+    if (status != 0) {
+        Serial.print("Unable to connect to host. Error: ");
         Serial.println(status);
     }
 }
 
-void gprsConnect() {
-    bool connected = false;
-    while (!connected) {
-        if (nbAccess.begin(PINNUMBER, SECRET_GPRS_APN, SECRET_GPRS_LOGIN, SECRET_GPRS_PASSWORD) == NB_READY) {
-            connected = true;
+void loop() {
+    yield();
+}
+
+/**
+ * Prints the modem name and version. Do not call this method before MODEM.begin() has been
+ * called, or the first send call with hang forever.
+ */
+void printModemVersion() {
+    String response;
+
+    MODEM.send("AT+GMM");
+    MODEM.waitForResponse(100, &response);
+    Serial.print(response);
+    Serial.print(" version ");
+
+    MODEM.send("ATI9");
+    MODEM.waitForResponse(100, &response);
+    Serial.println(response);
+}
+
+/**
+ * Connects to the network.
+ *
+ * @param attempts the number of times to retry in case of failure
+ * @param delayMillis the number of milliseconds to wait before each retry
+ * @return true on connection success, false otherwise
+ */
+bool gprsConnect(int attempts, int delayMillis) {
+    for (int i = 0; i < attempts; i++) {
+        if (nbAccess.begin(PINNUMBER, GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == NB_READY) {
+            return true;
         } else {
-            Serial.println("Not connected");
-            delay(1000);
+            delay(delayMillis);
         }
     }
+    return false;
+}
 
-    Serial.println("Connected to the network");
+/**
+ * Downloads https://${host}/${path} and prints it to Serial
+ *
+ * @param host
+ * @param path
+ * @return 0 on success, otherwise error code > 0
+ */
+int downloadSsl(const char *host, const char *path) {
+    if (!sslClient.connect(host, 443)) {
+        return ERROR_SSL_CONNECT;
+    }
+    if (!performGet(&sslClient, host, path)) {
+        return ERROR_REQUEST_FAILED;
+    }
+    if (copyStream(&sslClient, &Serial) == -1) {
+        return ERROR_RESPONSE_DOWNLOAD;
+    }
+    return 0;
+}
+
+/**
+ * Sends a GET request for path the the specified host. The Connection header is set to close.
+ *
+ * @param client
+ * @param host
+ * @param path
+ * @return true on success, false in case of error sending request
+ */
+bool performGet(Client *client, const char *host, const char *path) {
+
+    char requestLine[300];
+    snprintf(requestLine, sizeof(requestLine), "GET %s HTTP/1.1", path);
+    if (client->println(requestLine) == 0) {
+        return false;
+    }
+    snprintf(requestLine, sizeof(requestLine), "Host: %s", host);
+    if (client->println(requestLine) == 0) {
+        return false;
+    }
+    snprintf(requestLine, sizeof(requestLine), "Connection: close");
+    if (client->println(requestLine) == 0) {
+        return false;
+    }
+    client->println();
+    return true;
+}
+
+/**
+ * Copies bytes from source and prints them into target until there are no more bytes
+ * available in source. When source runs out of bytes you can optionally wait a bit for more
+ * data using waitForBytesAttempts and waitForBytesDelayMillis.
+ *
+ * @param source the Stream to read data from
+ * @param target the Stream to print data to
+ * @param waitForBytesAttempts the number of times to retry checking source for more data
+ * @param waitForBytesDelayMillis the number of millis to wait before each retry attempt
+ * @return the number of bytes copied, or -1 if any error during printing to target
+ */
+int copyStream(Stream *source, Stream *target, int waitForBytesAttempts, int waitForBytesDelayMillis) {
+    char c;
+    int bytesCopied = 0;
+    for (int i = 0; i < waitForBytesAttempts; i++) {
+        while (source->available()) {
+            c = source->read();
+            if (target->print(c) == 0) {
+                return -1;
+            }
+            bytesCopied++;
+            i = 0;
+        }
+        delay(waitForBytesDelayMillis);
+    }
+    return bytesCopied;
 }
