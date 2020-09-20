258c4de651ac79161ef9eec999a4137a07ca25cd
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-09-20 15:09:45 +0200

    Full example using the ArduinoBearSSL library for https instead of native SARA U201 features.

---
+++ gsm-https-letsencrypt/README.md
@@ -0,0 +1,21 @@
+# nb-https-letsencrypt
+
+## Description
+
+When I got the Arduino MKR NB 1500 from https://www.elfadistrelec.no/ it came with the U-Blox SARA-R410M-02B modem with 
+firmware/app version L0.0.00.00.05.06,A.02.00. Although the MKRNB@1.3.2 library will load a lot of commonly used
+SSL/TLS certificates, a lot of connections to servers using these certificates will still fail. I believe this has 
+to do with SSL/TLS version used by some servers not being supported by the U-Blox modem. The modem firmware and app
+version is not the newest available, but the process for accessing new firmware and updating the modem on the MRK boards
+is convoluted and a bit tricky, and I don't even know for sure if updating the firmware will solve the problem.
+
+To allow the MKR boards to make general use of https and access most servers, you can use the ArduinoBearSSL library
+for SSL/TLS instead of the implementation on the modem. This example shows how.
+
+The example (like all examples in this repo) is intended to be run with https://platformio.org so you will have to 
+install that first. Then create the `src/arduino_secrets.h` file based on the `src/default_secrets.h` file, connect 
+your Arduino MKR NB 1500 and run
+
+    pio run -t upload
+    
+The example will wait for you to connect to Serial, then connect to https://letsencrypt.org and print the response.
---
+++ gsm-https-letsencrypt/src/main.cpp
@@ -0,0 +1,114 @@
+#include <Arduino.h>
+#include <MKRGSM.h>
+#include <ArduinoBearSSL.h>
+#include "http_ssl.h"
+
+// Create the arduino_secrets.h file with the specifics for connecting to the network for
+// your provider.
+#if __has_include("arduino_secrets.h")
+
+#include "arduino_secrets.h"
+
+#else
+#include "default_secrets.h"
+#endif
+
+const char PINNUMBER[] = SECRET_PINNUMBER;
+const char GPRS_APN[] = SECRET_GPRS_APN;
+const char GPRS_LOGIN[] = SECRET_GPRS_LOGIN;
+const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
+
+GSM gsmAccess(false);
+GPRS gprsAccess;
+GSMClient gsmClient;
+
+// Create a BearSSLClient object, wrapping the NBClient. This is what will allow us to use
+// https connections without relying on the NBSSLClient in the MKRNB library.
+BearSSLClient sslClient(gsmClient);
+
+bool gprsConnect(int attempts=3, int delayMillis=1000);
+void printModemVersion();
+
+unsigned long getTime() {
+    return gsmAccess.getTime();
+}
+
+void setup() {
+    // Let's pull the builtin led down to prevent it from floating...
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
+    Serial.begin(9600);
+    while (!Serial) { delay(100); }
+
+    Serial.println("Connecting to network...");
+
+    if (!gprsConnect()) {
+        Serial.print("Unable to connect to network. Aborting.");
+        return;
+    }
+    Serial.println("Connected to network successfully...");
+
+    printModemVersion();
+
+    // Important step. Let BearSSL know how to get the current time.
+    // Needed for certificate verification.
+    ArduinoBearSSL.onGetTime(getTime);
+
+    // Download https://letsencrypt.org . This will not work using NBSSLClient on the
+    // u-blox SARA-R410M-02B modem with firmware/app version L0.0.00.00.05.06,A.02.00
+    // from Feb 03 2018. BearSSL to the rescue.
+    int status = downloadSsl(&sslClient, "letsencrypt.org", "/");
+    if (status != 0) {
+        Serial.print("Unable to connect to host. Error: ");
+        Serial.println(status);
+    }
+}
+
+void loop() {
+    // We are not doing anything besides what is done in setup().
+    yield();
+}
+
+/**
+ * Connects to the network.
+ *
+ * @param attempts the number of times to retry in case of failure
+ * @param delayMillis the number of milliseconds to wait before each retry
+ * @return true on connection success, false otherwise
+ */
+bool gprsConnect(int attempts, int delayMillis) {
+    for (int i = 0; i < attempts; i++) {
+        if (gsmAccess.begin(PINNUMBER) != GSM_READY) {
+            if (i == attempts-1) {
+                return false;
+            }
+            delay(delayMillis);
+        }
+    }
+    for (int i = 0; i < attempts; i++) {
+        if (gprsAccess.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY) {
+            return true;
+        } else {
+            delay(delayMillis);
+        }
+    }
+    return false;
+}
+
+/**
+ * Prints the modem name and version. Do not call this method before MODEM.begin() has been
+ * called, or the first send call with hang forever.
+ */
+void printModemVersion() {
+    String response;
+
+    MODEM.send("AT+GMM");
+    MODEM.waitForResponse(100, &response);
+    Serial.print(response);
+    Serial.print(" version ");
+
+    MODEM.send("ATI9");
+    MODEM.waitForResponse(100, &response);
+    Serial.println(response);
+}
